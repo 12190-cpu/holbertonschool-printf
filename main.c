02859f00afd30f2f4518e18deef680ae91f4ea38
Farid <12190@holbertonstudents.com>
2025-11-26 15:37:51 +0100

    Remove length output from main function
    Removed the printf statement that outputs the lengths.

--- main.c
+++ main.c
@@ -14,6 +14,6 @@ int main(void)
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-	printf("%d%d", len, len2);
+
 	return (0);
 }
